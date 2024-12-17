d932595f079cea800a764b69e1436a8d7d670b8f
Vynh-creator <hthfnbt@gmail.com>
2024-12-17 21:05:03 +0300

    Исправляю ошибки

--- app/database/models.py
+++ app/database/models.py
@@ -3,13 +3,12 @@ from sqlalchemy.log import echo_property
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 from sqlalchemy.sql.coercions import expect
-
+from sqlalchemy import text
 engine = create_async_engine(url="postgresql+asyncpg://postgres:456rty@localhost/tlg",echo=True)
 async_session = async_sessionmaker(engine)
 metadata=MetaData()
 from sqlalchemy.exc import SQLAlchemyError
 
-# days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
@@ -17,33 +16,35 @@ class Base(AsyncAttrs, DeclarativeBase):
 
 async def create_user_table():
     async with async_session() as session:
-        await session.execute("""
+        await session.execute(text("""
         CREATE TABLE IF NOT EXISTS public.user_inf
 (
     user_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     tg_id bigint,
-    first_name_p character varying(20) COLLATE pg_catalog."default",
-    last_name_p character varying(20) COLLATE pg_catalog."default",
-    phone character varying(20) COLLATE pg_catalog."default",
+    first_name_p character varying(20) ,
+    last_name_p character varying(20) ,
+    phone character varying(20) ,
     chat_id bigint,
     CONSTRAINT pk_book_book_id PRIMARY KEY (user_id)
 )
-        """)
+        """))
         await session.commit()
 
 
 
 async def create_lesson_day_table():
     async with async_session() as session:
-        await session.execute("""
-        CREATE TABLE IF NOT EXISTS public.lesson_day
-(
-    day_id character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    state_fill boolean NOT NULL DEFAULT false,
-    CONSTRAINT lesson_day_pkey PRIMARY KEY (day_id)
-)
-        """)
-        await session.commit()
+        async with session.begin():
+
+
+                await session.execute(text("""
+                CREATE TABLE IF NOT EXISTS public.lesson_day (
+                    day_id character varying(20) NOT NULL,
+                    state_fill boolean NOT NULL DEFAULT false,
+                    CONSTRAINT lesson_day_pkey PRIMARY KEY (day_id)
+                )
+                """))
+                await session.commit()
 
 
 
@@ -55,12 +56,16 @@ async def insert_into_lesson_day(days: list[str]):
         async with session.begin():
             try:
                 for day in days:
-                    await session.execute("""
+                    result=await session.execute(text("""
+                    SELECT COUNT(*) FROM lesson_day WHERE day_id = :day
+                    """),{"day":day})
+                    if result.scalars()==0:
+                        await session.execute(text("""
                     INSERT INTO lesson_day (day_id)
                     VALUES (:day_id)
-                    """, {'day_id': day})
+                    """), {'day_id': day})
 
-                await session.commit()
+                    await session.commit()
             except SQLAlchemyError as e:
                 await session.rollback()
                 print(f"Error occurred: {e}")
@@ -69,37 +74,35 @@ async def create_table_days(days: list[str]):
     async with async_session() as session:
         async with session.begin():
             try:
-
-               for day in days:
-                   table_name=f"public.{day}"
-                   await session.execute(f"""
-                CREATE TABLE IF NOT EXISTS {table_name}
-(
-    state_fill boolean DEFAULT false,
-    time_less character varying(20) COLLATE pg_catalog."default",
-    time_id serial PRIMARY KEY,
-
-)
-
-                """)
-                   await session.commit()
+                for day in days:
+                    table_name = f"public.{day}"
+                    await session.execute(text(f"""
+                        CREATE TABLE IF NOT EXISTS {table_name}
+                        (
+                            state_fill boolean DEFAULT false,
+                            time_less character varying(20),
+                            time_id serial PRIMARY KEY
+                        )
+                    """))
+                await session.commit()
             except Exception as e:
                 await session.rollback()
-                print(f'Error occured: {e}')
+                print(f'Error occurred: {e}')
+
 
 
 async def create_time_ch_tables():
     async with async_session() as session:
         async with session.begin():
-            await session.execute(""" 
+            await session.execute(text(""" 
             CREATE TABLE IF NOT EXISTS public.time_ch
 (
     time_ch_id integer NOT NULL DEFAULT nextval('time_ch_time_ch_id_seq'::regclass),
-    day_name character varying(20) COLLATE pg_catalog."default",
-    "time" character varying(30) COLLATE pg_catalog."default",
+    day_name character varying(20) ,
+    "time" character varying(30) ,
     child_id integer
 )
-            """)
+            """))
             await session.commit()
 
 
@@ -107,33 +110,43 @@ async def insert_into_days_table(times: list[str]):
     async with async_session() as session:
         async with session.begin():
             try:
-               for time in times:
-                   table_name=f'public.{time.split('_')[1]}'
-                   await session.execute(f""" 
-                   INSERT INTO {table_name} (time_less)
-                   VALUES (:time)
-                   """,{'time':time})
-                   await session.commit()
+                for time in times:
+                    table_name = f'public.{time.split("_")[1]}'
+
+
+                    result = await session.execute(text(f"""
+                        SELECT COUNT(*) FROM {table_name} WHERE time_less = :time
+                    """), {'time': time})
+
+                    count = result.scalar()
+
+                    if count == 0:
+                        await session.execute(text(f"""
+                            INSERT INTO {table_name} (time_less)
+                            VALUES (:time)
+                        """), {'time': time})
+
+                await session.commit()
             except Exception as e:
                 await session.rollback()
-                print(f'Exception occured {e}')
+                print(f'Exception occurred: {e}')
 
 
-async def create_time_table(times: list[list[str]]):
+async def create_time_table(times: list[str]):
     async with async_session() as session:
         async with session.begin():
             try:
                for time in times:
-                   table_name=f'public.{time}'
-                   await session.execute(f""" 
+                   table_name = f'public."{time}"'
+                   await session.execute(text(f""" 
    CREATE TABLE IF NOT EXISTS {table_name}
 (
-    id_pers NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 12 CACHE 1 ),
+    id_pers integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 12 CACHE 1 ),
     tg_id bigint,
     child_id integer,
-    CONSTRAINT fk_id_pers_id_pers FOREIGN KEY (child_id)
+    CONSTRAINT fk_id_pers_id_pers FOREIGN KEY (child_id) REFERENCES public.childs (child_id)
 
-)""")
+)"""))
 
                    await session.commit()
             except Exception as e:
@@ -144,7 +157,7 @@ async def create_childs_table():
     async with async_session() as session:
         async with session.begin():
             try:
-                session.execute("""
+                await session.execute(text("""
                 CREATE TABLE IF NOT EXISTS public.childs
 (
     child_id integer NOT NULL DEFAULT nextval('childs_child_id_seq'::regclass),
@@ -159,7 +172,8 @@ async def create_childs_table():
         ON DELETE NO ACTION
 )
 
-                """)
+                """))
+                await session.commit()
             except Exception as e:
                 await session.rollback()
                 print(f'Exception occured {e}')
@@ -171,6 +185,4 @@ async def create_childs_table():
 
 
 
-async def async_main():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+
--- main.py
+++ main.py
@@ -5,25 +5,39 @@ import logging
 
 from app.handlers import router
 
-from app.database.models import async_main,create_table_days,create_time_table,create_user_table,create_childs_table,create_time_ch_tables,create_lesson_day_table,insert_into_days_table,insert_into_lesson_day
-
-
+from app.database.models import create_table_days,create_time_table,create_user_table,create_childs_table,create_time_ch_tables,create_lesson_day_table,insert_into_days_table,insert_into_lesson_day
+days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+clock = [
+    ["12:30_Понедельник", "13:30_Понедельник"],
+    ["12:30_Вторник", "13:30_Вторник"]
+]
+bot = Bot(token="8013109178:AAHGZ_if1VPNtsG2PmHG7KSZN5wo0NvtDlk")
+dp = Dispatcher()
+dp.include_router(router)
 
 async def main():
-    await async_main()
-    bot = Bot(token="8013109178:AAHGZ_if1VPNtsG2PmHG7KSZN5wo0NvtDlk")
-    dp = Dispatcher()
-    dp.include_router(router)
+
+    await create_childs_table()
+    await create_lesson_day_table()
+    await create_time_table(sum(clock, []))
+    await create_table_days(days)
+    await create_user_table()
+    await create_time_ch_tables()
+    await insert_into_days_table(sum(clock, []))
+    await  insert_into_lesson_day(days)
+
     await dp.start_polling(bot)
+
+
+
+
 
 
 if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO)
     try:
-        asyncio.run(create_childs_table())
-        asyncio.run(create_lesson_day_table())
-        asyncio.run(create_time_table())
+
         asyncio.run(main())
     except KeyboardInterrupt:
         print("Exit")
